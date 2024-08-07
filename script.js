a8930ed5a67aa72827078657918ccedce3add942
lancer385 <ahmedkhalaf1234123@gmail.com>
2024-08-07 16:37:23 +0300

    add comments

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function gameBoard(){
   let board;
   const gridSize = 3;
   let cells;
+  // handles creating the game board  
   const makeBoard = ()=> {
     board = [];
     cells = 1;
@@ -13,19 +14,24 @@ function gameBoard(){
       }
     };
   }
+  // initial call to create the board
   makeBoard();
+  // gets the updated board
   const getBoard = () => board;
-  const placeMark = (row, column, player) => {
+  // handles inputing players' symbols
+  const addSymbol = (row, column, player) => {
       board[row][column] = player;
   };
   return {
     getBoard,
-    placeMark,
+    addSymbol,
+    // this is used for the retry button
     resetBoard: makeBoard};
 };
 
 
 function gameController(){
+  // stores players data
   const players = [
     {
       name: "Player One",
@@ -38,7 +44,9 @@ function gameController(){
       score: 0
     }
   ];
+  // gets the reuired methods to change the board
   const board = gameBoard();
+  // count the turns to check when there's a tie which is at the ninth turn
   let turnCounter = 0;
   const incraseCounter = () => {
     turnCounter++;
@@ -46,12 +54,18 @@ function gameController(){
   const resetCounter = () => {
     turnCounter = 0;
   };
+  const getTurnCounter = () => turnCounter;
+
+  // method to switch players' turns
   let activePlayer = players[0];
   const switchTurn = () => {
   activePlayer = (activePlayer === players[0])? players[1] : players[0];
   };
-  const getTurnCounter = () => turnCounter;
   const getActivePlayer = () => activePlayer;
+
+
+
+  // the logic that handles the winning conditions
   const isRowEqual = arr => arr.every(val => val === arr[0]);
   const isColEqual = (board, col) => {
     let colCells = board.map(row => row[col]);
@@ -69,6 +83,7 @@ function gameController(){
     let checkDiag = diag => diag.every(val => val === diag[0]);
     return checkDiag(diagCells) || checkDiag(diagCells2);
   };
+  // if one of the conditions are met, then the game is over!
   const isGameOver = () => {
     let getBoard = board.getBoard()
     for (i = 0; i < getBoard.length; i++){
@@ -84,8 +99,12 @@ function gameController(){
     };
     return false;
   };
+
+
+
+  // handles each player turn
   const playARound = (row, column) => {
-      board.placeMark(row, column, getActivePlayer().symbol)
+      board.addSymbol(row, column, getActivePlayer().symbol)
       if (!isGameOver()){
       switchTurn();
       incraseCounter();
@@ -94,6 +113,7 @@ function gameController(){
         getActivePlayer().score++;
       }
   };
+  // all the returned methods that are needed for the UI
   return {
     playerOne:players[0],
     playerTwo:players[1],
@@ -110,7 +130,11 @@ function gameController(){
 
 function screenController(){
   const boardDiv = document.querySelector(".board");
+  // displays the game status such as the player's turn and winning condition
   const status = document.querySelector(".status");
+
+
+  // displays each player score and name
   const scores = {
     cross: document.querySelector(".cross"),
     circle: document.querySelector(".circle")
@@ -119,24 +143,29 @@ function screenController(){
     cross: document.querySelector(".one"),
     circle: document.querySelector(".two")
   };
+
+
   const retryButton = document.querySelector(".retry");
   const game = gameController();
   let board = game.getBoard();
   const updateScreen = ()=> {
+    // this part is needed to update the board whenever you retry
     board = game.getBoard();
     status.innerHTML = `${game.getActivePlayer().name}<br>TURN`;
     boardDiv.textContent = "";
+
+    // handles disaplying the symbols in each cell
     board.forEach((row, rowIndex) => { 
       row.forEach((cell, colIndex) =>{
         const cellB = document.createElement("button");
         cellB.classList.add("cell");
         cellB.dataset.row = rowIndex;
         cellB.dataset.column = colIndex;
-        // only display the players symbol
+        // only displays the players symbol. any other value will be disgarded
         if (cell === game.playerOne.symbol || cell === game.playerTwo.symbol){
           cellB.textContent = cell;
         };
-        // this is for making a different color for x
+        // this is just for styling, making different color for X
         if (cellB.textContent === "X"){
           cellB.classList.add("unique");
         }
@@ -157,13 +186,17 @@ function screenController(){
     scores.cross.innerHTML = `X<br>${game.playerOne.score}`;
     scores.circle.innerHTML = `O<br>${game.playerTwo.score}`;
   };
-  function insertSymbol(e){
+
+  // click handler for adding player's symbol 
+  function inputClicker(e){
     const column = e.target.dataset.column;
     const row = e.target.dataset.row;
     if (!row && !column || (board[row][column] === game.playerOne.symbol || board[row][column] === game.playerTwo.symbol) || game.isGameOver()) {return};
       game.playARound(row, column);
       updateScreen();
   };
+
+
   function retry(){
     game.resetBoard();
     game.isGameOver();
@@ -173,6 +206,8 @@ function screenController(){
     game.resetCounter();
     updateScreen();
   };
+
+  // handles renaming players
   function rename(player){
     let userPrompt = prompt("Insert Your Name Here");
     if (userPrompt !== null){
@@ -183,7 +218,7 @@ function screenController(){
   names.cross.addEventListener("click", () => rename(game.playerOne));
   names.circle.addEventListener("click", () => rename(game.playerTwo));
   retryButton.addEventListener("click", retry);
-  boardDiv.addEventListener("click", insertSymbol);
+  boardDiv.addEventListener("click", inputClicker);
   updateScreen();
 };
 screenController();
